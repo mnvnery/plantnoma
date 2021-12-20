52c08182dd9cf5065b855b56b48a98a7fe09f455
Maria Nery <67118326+mnvnery@users.noreply.github.com>
2021-12-20 11:05:28 +0000

    menu page

--- _site/assets/js/scripts.js
+++ _site/assets/js/scripts.js
@@ -1,6 +1,6 @@
 $('.hamburger').click(function() {
     $('.hamburger').toggleClass("is-active");
-    $('.hamburger-inner').toggleClass("dark-mode");
+    $('.hamburger-inner, .logo-elem').toggleClass("dark-mode");
     $('.navbar-container').toggleClass('navbar-open');
 });
 
@@ -9,34 +9,6 @@ $('a .book-btn').hover(function() {
 });
 
 gsap.registerPlugin(ScrollTrigger);
-
-ScrollTrigger.defaults({
-    markers: true
-});
-/*
-$(".circle").each(function (index) {
-    let triggerElement = $(this);
-    let targetElement = $(this);
-
-    let tl = gsap.timeline({
-        scrollTrigger: {
-            trigger: triggerElement,
-            // trigger element - viewport
-            start: "top 80%",
-            end: "top 30%",
-            scrub: true,
-            pin: true
-        }
-    });
-    tl.to(targetElement, {
-            width: "116vw",
-            position: "absolute",
-            bottom: "-62vh",
-            right: "-9vw"
-    });
-});
-
-*/
 
 gsap.to(".circle", {
     width: "41vw",
@@ -51,83 +23,89 @@ gsap.to(".circle", {
 })
 
 // Animate From To
-$(".box-1").each(function (index) {
-    let triggerElement = $(this);
-    let targetElement = $(".greens");
-  
-    let tl = gsap.timeline({
-      scrollTrigger: {
-        trigger: triggerElement,
-        // trigger element - viewport
-        start: "bottom 65%",
-        end: "bottom 10%",
-        scrub: true
-      }
-    });
-    tl.fromTo(targetElement, {
-        y:550,
-        duration: 1
-      },
-      {
-        y:-100,
-        duration: 1
-      }
-    );
-  });
+
+let tl = gsap.timeline({
+    scrollTrigger: {
+    trigger: $(".box-1"),
+    // trigger element - viewport
+    start: "bottom 65%",
+    end: "bottom 10%",
+    scrub: true
+    }
+});
+tl.fromTo($(".greens"), {
+    y:550,
+    duration: 1
+    },
+    {
+    y:-100,
+    duration: 1
+    }
+);
+
 
 // Animate From To
-$(".box-1").each(function (index) {
-    let triggerElement = $(this);
-    let targetElement = $(".living-world");
-  
-    let tl = gsap.timeline({
-      scrollTrigger: {
-        trigger: triggerElement,
-        // trigger element - viewport
-        start: "bottom 65%",
-        end: "bottom 10%",
-        scrub: true
-      }
-    });
-    tl.fromTo(targetElement, {
-        y:550,
-        duration: 1
-      },
-      {
-        y:0,
-        duration: 1
-      }
-    );
-});
 
-  // Animate From To
-$(".box-1").each(function (index) {
-    let triggerElement = $(this);
-    let targetElement = $(".human")
 
-    let tl = gsap.timeline({
+let tl1 = gsap.timeline({
     scrollTrigger: {
-        trigger: triggerElement,
-        // trigger element - viewport
-        start: "bottom 50%",
-        end: "bottom 50%",
-        scrub: true,
-        onEnter: () => {
-            $(".here").removeClass("is--active");
-            targetElement.addClass("is--active");
-        },
-        onEnterBack: () => {
-            targetElement.removeClass("is--active");
-            $(".here").addClass("is--active");
-        }
-        }
-    });   
+    trigger: ".box-1",
+    // trigger element - viewport
+    start: "bottom 65%",
+    end: "bottom 10%",
+    scrub: true
+    }
 });
+tl1.fromTo($(".living-world"), {
+    y:550,
+    duration: 1
+    },
+    {
+    y:0,
+    duration: 1
+    }
+);
+
+
+// Animate From To
+
+let timeline = gsap.timeline({
+scrollTrigger: {
+    trigger: $(".box-1"),
+    // trigger element - viewport
+    start: "bottom 50%",
+    end: "bottom 50%",
+    scrub: true,
+    onEnter: () => {
+        $(".here").removeClass("is--active");
+        $(".human").addClass("is--active");
+    },
+    onEnterBack: () => {
+        $(".human").removeClass("is--active");
+        $(".here").addClass("is--active");
+    }
+    }
+});   
+
+    
 
 // background color change - garden
 
 gsap.to("body", {
     backgroundColor: "#38523E",
+    scrollTrigger: {
+        start: "bottom 60%",
+        end: "bottom 50%",
+        trigger: ".box-2",
+        scrub: true,
+    }
+})
+
+
+gsap.to(".circle-2", {
+    display: "block",
+    width: '104vw',
+    y: 5,
     scrollTrigger: {
         start: "bottom 100%",
         end: "bottom 50%",
@@ -135,6 +113,7 @@ gsap.to("body", {
         scrub: true,
     }
 })
+
 
 
 gsap.to(".logo-elem", {
@@ -158,30 +137,240 @@ gsap.to(".static-line", {
     }
 })
 
-gsap.to(".living-world", {
+gsap.to(".living-world, .greens", {
     opacity: "0",
     scrollTrigger: {
         start: "bottom 100%",
-        end: "bottom 50%",
+        end: "bottom 60%",
         trigger: ".box-2",
         scrub: true,
     }
 })
 
-let tl = gsap.timeline({
+
+
+let tl2 = gsap.timeline({
+scrollTrigger: {
+    trigger: $(".box-2"),
+    // trigger element - viewport
+    start: "bottom 80%",
+    end: "bottom 80%",
+    scrub: true,
+    onEnter: () => {
+        $(".human").removeClass("is--active");
+        $(".garden").addClass("is--active");
+    },
+    onEnterBack: () => {
+        $(".garden").removeClass("is--active");
+        $(".human").addClass("is--active");
+    }
+    }
+});   
+
+gsap.to(".hamburger-inner", {
     scrollTrigger: {
         trigger: ".box-2",
         // trigger element - viewport
-        start: "bottom 100%",
-        end: "bottom 50%",
+        start: "bottom 60%",
+        end: "bottom 60%",
         scrub: true,
         onEnter: () => {
-            $(".human").removeClass("is--active");
-            $(".garden").addClass("is--active");
+            $(".hamburger-inner").addClass("dark-mode");
         },
         onEnterBack: () => {
-            $(".garden").removeClass("is--active");
-            $(".human").addClass("is--active");
+            $(".hamburger-inner").removeClass("dark-mode");
         }
+    }
+})
+
+window.addEventListener("load", function() {
+    // ARE YOU HERE
+
+    gsap.to("body", {
+        backgroundColor: "#EBEEEC",
+        immediateRender: false,
+        scrollTrigger: {
+            trigger: ".box-3",
+            start: "top bottom",
+            end: "center bottom",
+            scrub: true, 
         }
-    });   
+    })
+
+    gsap.to(".static-line", {
+        borderColor: "#38523E",
+        immediateRender: false,
+        scrollTrigger: {
+            start: "top bottom",
+            end: "center bottom",
+            trigger: ".box-3",
+            scrub: true,
+        }
+    })
+
+    gsap.to(".logo-elem", {
+        fill: "#38523E",
+        immediateRender: false,
+        scrollTrigger: {
+            start: "top bottom",
+            end: "center bottom",
+            trigger: ".box-3",
+            scrub: true,
+        }
+    })
+
+    gsap.to(".circle-2, .circle", {
+        opacity: "0",
+        scrollTrigger: {
+            trigger: ".box-3",
+            start: "top bottom",
+            end: "top bottom",
+            scrub: true, 
+        }
+    })
+
+    gsap.to(".areyouhere", {
+        scrollTrigger: {
+            trigger: ".box-3",
+            // trigger element - viewport
+            start: "top bottom",
+            end: "top bottom",
+            scrub: true,
+            onEnter: () => {
+                $(".garden").removeClass("is--active");
+                $(".areyouhere").addClass("is--active");
+            },
+            onEnterBack: () => {
+                $(".areyouhere").removeClass("is--active");
+                $(".garden").addClass("is--active");
+            }
+        }
+    })
+
+    gsap.to(".hamburger-inner", {
+        scrollTrigger: {
+            trigger: ".box-3",
+            // trigger element - viewport
+            start: "top bottom",
+            end: "top bottom",
+            scrub: true,
+            onEnter: () => {
+                $(".hamburger-inner").removeClass("dark-mode");
+            },
+            onEnterBack: () => {
+                $(".hamburger-inner").addClass("dark-mode");
+            }
+        }
+    })
+
+    gsap.from(".events-container", {
+        y: 550,
+        scrollTrigger: {
+            start: "top bottom",
+            end: "80% bottom",
+            trigger: ".box-3",
+            scrub: true,
+        }
+    })
+    
+    gsap.from(".compost-images", {
+        y: 400,
+        scrollTrigger: {
+            trigger: ".box-4",
+            start: "top 90%",
+            end: "center bottom",
+            scrub: true,
+        }
+    })
+    gsap.to("body", {
+        backgroundColor: "#38523E",
+        scrollTrigger: {
+            start: "40% 90%",
+            end: "80% bottom",
+            trigger: ".box-4",
+            scrub: true,
+        }
+    })
+    
+    gsap.to(".logo-elem", {
+        fill: "#EBEEEC",
+        scrollTrigger: {
+            start: "40% bottom",
+            end: "80% bottom",
+            trigger: ".box-4",
+            scrub: true,
+        }
+    })
+    
+    
+    gsap.to(".static-line", {
+        borderColor: "#EBEEEC",
+        scrollTrigger: {
+            start: "40% bottom",
+            end: "80% bottom",
+            trigger: ".box-4",
+            scrub: true,
+        }
+    })
+    gsap.to(".events-container", {
+        opacity: "0",
+        scrollTrigger: {
+            start: "35% 90%",
+            end: "70% bottom",
+            trigger: ".box-4",
+            scrub: true,
+        }
+    })
+
+    gsap.to(".hereyouare", {
+        scrollTrigger: {
+            trigger: ".box-4",
+            // trigger element - viewport
+            start: "30% 90%",
+            end: "30% 90%",
+            scrub: true,
+            onEnter: () => {
+                $(".areyouhere").removeClass("is--active");
+                $(".hereyouare").addClass("is--active");
+            },
+            onEnterBack: () => {
+                $(".hereyouare").removeClass("is--active");
+                $(".areyouhere").addClass("is--active");
+            }
+        }
+    })
+    gsap.to(".compost-images", {
+        y: -740,
+        immediateRender: false,
+        scrollTrigger: {
+            start: "bottom 70%",
+            end: "bottom -8%",
+            trigger: ".box-4",
+            scrub: true,
+
+        }
+    })
+    gsap.to("body", {
+        backgroundColor: "#EBEEEC",
+        immediateRender: false,
+        scrollTrigger: {
+            start: "55% bottom",
+            end: "top 35%",
+            trigger: ".footer-page",
+            scrub: true,
+        }
+    })
+
+    gsap.to(".logo-elem", {
+        fill: "#38523E",
+        immediateRender: false,
+        scrollTrigger: {
+            start: "55% bottom",
+            end: "top 35%",
+            trigger: ".footer-page",
+            scrub: true,
+            markers: true
+        }
+    })
+});
+
