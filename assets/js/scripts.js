565da089db7b9b9f77a1ba2086c55fee400cfa02
Maria Nery <67118326+mnvnery@users.noreply.github.com>
2021-12-22 15:14:25 +0000

    bugs

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,6 +1,6 @@
 $('.hamburger').click(function() {
     $('.hamburger').toggleClass("is-active");
-    $('.hamburger-inner, .logo-elem').toggleClass("dark-mode");
+    $('.hamburger-inner, .logo-elem').toggleClass("light-background");
     $('.navbar-container').toggleClass('navbar-open');
 });
 
@@ -9,3 +9,8 @@ $('a .book-btn').hover(function() {
 });
 
 
+$('.navbar a').click(function() {
+    $('.hamburger').toggleClass("is-active");
+    $('.navbar-container').toggleClass('navbar-open');
+    $('.hamburger-inner, .logo-elem').toggleClass("light-background");
+});
